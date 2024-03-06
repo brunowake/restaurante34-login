8c9a49c87a349961499352bcfb5bdd464f8a1616
Bruno Wake <brunowake.bw@gmail.com>
2024-03-06 18:13:29 -0300

    test: updating file

--- signUp/index.mjs
+++ signUp/index.mjs
@@ -1,38 +1,38 @@
 import { CognitoIdentityProvider } from "@aws-sdk/client-cognito-identity-provider";
 
 export const handler = async (event) => {
-    const cognitoIdentityProvider = new CognitoIdentityProvider({ region: 'us-east-1' });
-    const { username, password, name, email, cpf } = JSON.parse(event.body);
-    const clienteID = process.env.clienteID;
+  const cognitoIdentityProvider = new CognitoIdentityProvider({ region: 'us-east-1' });
+  const { username, password, name, email, cpf } = JSON.parse(event.body);
+  const clienteID = process.env.clienteID;
 
-    const params = {
-        ClientId: clienteID,
-        Username: username,
-        Password: password,
-        UserAttributes: [
-            {
-                Name: 'name',
-                Value: name
-            },
-            {
-                Name: 'email',
-                Value: email
-            },
-            {
-                Name: 'custom:cpf',
-                Value: cpf
-            }
-        ]
-    };
-    
-    try {
-        const data = await cognitoIdentityProvider.signUp(params);
-        return { statusCode: 200, body: JSON.stringify(data) };
-    } catch (error) {
-        console.error('Error registering user:', error);
-        return { statusCode: 500, body: JSON.stringify({ error: error.message }) };
-    }
-    
+  const params = {
+    ClientId: clienteID,
+    Username: username,
+    Password: password,
+    UserAttributes: [
+      {
+        Name: 'name',
+        Value: name
+      },
+      {
+        Name: 'email',
+        Value: email
+      },
+      {
+        Name: 'custom:cpf',
+        Value: cpf
+      }
+    ]
+  };
+
+  try {
+    const data = await cognitoIdentityProvider.signUp(params);
+    return { statusCode: 200, body: JSON.stringify(data) };
+  } catch (error) {
+    console.error('Error registering user:', error);
+    return { statusCode: 500, body: JSON.stringify({ error: error.message }) };
+  }
+
 };
 /**
  * payload signup
@@ -40,6 +40,7 @@ export const handler = async (event) => {
  * {
     "username": "usuario",
     "password": "senha",
+    
     "name": "Nome Teste",
     "email": "email@gmail.com",
     "cpf": "11111111111"
