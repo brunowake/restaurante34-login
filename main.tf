5b5ba56b2302285af0086c81a614e0f092b4c571
Bruno Wake <brunowake.bw@gmail.com>
2024-03-06 17:26:31 -0300

    fix: tf variables

--- main.tf
+++ main.tf
@@ -22,13 +22,13 @@ variable "AWS_ACCESS_KEY_ID" {
    type = string
 }
 
-variable "TF_VAR_AWS_SECRET_ACCESS_KEY" {
+variable "AWS_SECRET_ACCESS_KEY" {
    type = string
 }
 provider "aws" {
   region  = "us-east-1"
-  access_key = TF_VAR_AWS_ACCESS_KEY_ID
-  secret_key = TF_VAR_AWS_SECRET_ACCESS_KEY
+  access_key = var.AWS_ACCESS_KEY_ID
+  secret_key = var.AWS_SECRET_ACCESS_KEY
 }
 
 
