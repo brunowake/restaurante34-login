34eab94eaaf917b2d40320de24f8489106ab097b
Bruno Wake <brunowake.bw@gmail.com>
2024-03-06 17:06:57 -0300

    fix: variables

--- main.tf
+++ main.tf
@@ -19,7 +19,10 @@ terraform {
 }
 provider "aws" {
   region  = "us-east-1"
+  access_key = var.AWS_ACCESS_KEY_ID
+  secret_key = var.AWS_SECRET_ACCESS_KEY
 }
+
 
 data "archive_file" "lambda_terraform_signUp" {
   type = "zip"
