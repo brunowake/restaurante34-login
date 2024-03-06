65124e45713892b91bc887cb436efeb4eecbf40d
Bruno Wake <brunowake.bw@gmail.com>
2024-03-06 17:11:55 -0300

    fix: variables name

--- main.tf
+++ main.tf
@@ -19,8 +19,8 @@ terraform {
 }
 provider "aws" {
   region  = "us-east-1"
-  access_key = TF_VAR_AWS_ACCESS_KEY_ID
-  secret_key = TF_VAR_AWS_SECRET_ACCESS_KEY
+  access_key = var.TF_VAR_AWS_ACCESS_KEY_ID
+  secret_key = var.TF_VAR_AWS_SECRET_ACCESS_KEY
 }
 
 
