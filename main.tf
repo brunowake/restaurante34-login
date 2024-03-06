dbaaaee4b08fb6f7e59f6ff1e67758e26eb2b677
Bruno Wake <brunowake.bw@gmail.com>
2024-03-06 17:10:05 -0300

    fix: tf file

--- main.tf
+++ main.tf
@@ -19,8 +19,8 @@ terraform {
 }
 provider "aws" {
   region  = "us-east-1"
-  access_key = var.AWS_ACCESS_KEY_ID
-  secret_key = var.AWS_SECRET_ACCESS_KEY
+  access_key = TF_VAR_AWS_ACCESS_KEY_ID
+  secret_key = TF_VAR_AWS_SECRET_ACCESS_KEY
 }
 
 
