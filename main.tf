987a06333c5151d25857a3ca854189eddfbab08a
Bruno Wake <brunowake.bw@gmail.com>
2024-03-06 15:49:10 -0300

    chore: lambda setup cicd

---
+++ main.tf
@@ -0,0 +1,52 @@
+terraform {
+  required_version = ">= 0.12"
+  required_providers {
+    aws = ">= 4.10"
+    archive = {
+      source  = "hashicorp/archive"
+      version = "~> 2.2.0"
+    }
+  }
+
+}
+provider "aws" {
+  region  = "us-east-1"
+}
+
+data "archive_file" "lambda_terraform_login" {
+  type = "zip"
+
+  source_dir  = "${path.module}/signUp"
+  output_path = "${path.module}/signUp.zip"
+}
+
+# to Create function
+resource "aws_lambda_function" "terraform_test" {
+  function_name = "signUp"
+  filename      = "signUp.zip"
+  runtime = "nodejs20.x"
+  handler = "index.handler"
+  source_code_hash = data.archive_file.lambda_terraform_login.output_base64sha256
+  role             = aws_iam_role.lambda_exec.arn
+}
+
+resource "aws_iam_role" "lambda_exec" {
+  name = "serverless_test_lambda"
+  assume_role_policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [{
+      Action = "sts:AssumeRole"
+      Effect = "Allow"
+      Sid    = ""
+      Principal = {
+        Service = "lambda.amazonaws.com"
+      }
+      }
+    ]
+  })
+}
+
+resource "aws_iam_role_policy_attachment" "lambda_policy" {
+  role       = aws_iam_role.lambda_exec.name
+  policy_arn = "arn:aws:iam::aws:policy/service-role/AWSLambdaBasicExecutionRole"
+}
---
+++ signIn/index.mjs
@@ -0,0 +1,36 @@
+import { CognitoIdentityProvider } from "@aws-sdk/client-cognito-identity-provider";
+
+export const handler = async (event) => {
+    const cognitoIdentityProvider = new CognitoIdentityProvider({ region: 'us-east-1' });
+    const { username, password } = JSON.parse(event.body);
+    const clienteID = process.env.clienteID;
+
+    const params = {
+        AuthFlow: 'USER_PASSWORD_AUTH',
+        ClientId: clienteID,
+        AuthParameters: {
+            USERNAME: username,
+            PASSWORD: password,
+            SECRET_KEY: "true"
+        }
+    };
+    
+    try {
+        const data = await cognitoIdentityProvider.initiateAuth(params);
+        return { statusCode: 200, body: JSON.stringify(data) };
+    } catch (error) {
+        console.error('Error registering user:', error);
+        return { statusCode: 500, body: JSON.stringify({ error: error.message }) };
+    }
+    
+};
+
+
+/**
+ * payload signup
+ * 
+ * {
+    "username": "usuario",
+    "password": "senha",
+  }
+ */
