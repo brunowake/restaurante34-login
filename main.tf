ced45a4b35687cc49215527fa812f4cb8aabb076
Bruno Wake <brunowake.bw@gmail.com>
2024-03-11 21:22:55 -0300

    fix: misspelling and adding git ignore

--- main.tf
+++ main.tf
@@ -81,7 +81,7 @@ resource "aws_api_gateway_method" "restaurante34-api_confirmSignUp_method" {
 
 resource "aws_api_gateway_integration" "restaurante34-api_confirmSignUp_integration" {
   rest_api_id = aws_api_gateway_rest_api.restaurante34-api.id
-  resource_id = aws_api_gateway_resource.api_confirmSignUp_resource.id
+  resource_id = aws_api_gateway_resource.restaurante34-api_confirmSignUp_resource.id
   http_method = aws_api_gateway_method.restaurante34-api_confirmSignUp_method.http_method
   type        = "AWS_PROXY"
   integration_http_method = "POST"
