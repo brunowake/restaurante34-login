b290875eb41e5e578068f24ec9877393e902857b
Bruno Wake <brunowake.bw@gmail.com>
2024-03-18 20:47:04 -0300

    fix: js to ts

---
+++ confirmSignUp/index.ts
@@ -0,0 +1,31 @@
+import { CognitoIdentityProvider } from "@aws-sdk/client-cognito-identity-provider"
+
+export const handler = async (event) => {
+  const cognitoIdentityProvider = new CognitoIdentityProvider({
+    region: "us-east-1",
+  })
+  const { username, confirmationCode } = JSON.parse(event.body)
+  const clienteID = process.env.clienteID
+
+  const params = {
+    ClientId: clienteID,
+    Username: username,
+    ConfirmationCode: confirmationCode,
+  }
+
+  try {
+    const data = await cognitoIdentityProvider.confirmSignUp(params)
+    return { statusCode: 200, body: JSON.stringify(data) }
+  } catch (error) {
+    console.error("Error registering user:", error)
+    return { statusCode: 500, body: JSON.stringify({ error: error.message }) }
+  }
+}
+/**
+ * payload signup
+ * 
+ * {
+    "username": "teste",
+    "confirmationCode": "102030"
+  }
+ */
