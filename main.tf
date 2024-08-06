68f5cce0bb8f2c86fd5f54b5161d3d34958d85e4
bruno wake <brunowake.bw@gmail.com>
2024-08-06 20:44:50 -0300

    adicionar lambda excluir usuario

--- main.tf
+++ main.tf
@@ -117,6 +117,36 @@ resource "aws_lambda_permission" "terraform_lambda_permission_getUsers" {
   source_arn = "${aws_api_gateway_rest_api.restaurante34-api.execution_arn}/*/*"
 }
 
+resource "aws_api_gateway_resource" "restaurante34-api_delete_resource" {
+  rest_api_id = aws_api_gateway_rest_api.restaurante34-api.id
+  parent_id   = aws_api_gateway_rest_api.restaurante34-api.root_resource_id
+  path_part   = "delete"  
+}
+
+resource "aws_api_gateway_method" "restaurante34-api_delete_method" {
+  rest_api_id   = aws_api_gateway_rest_api.restaurante34-api.id
+  resource_id   = aws_api_gateway_resource.restaurante34-api_delete_resource.id
+  http_method   = "POST"  
+  authorization = "NONE" 
+}
+resource "aws_api_gateway_integration" "restaurante34-api_delete" {
+  rest_api_id = aws_api_gateway_rest_api.restaurante34-api.id
+  resource_id = aws_api_gateway_resource.restaurante34-api_delete_resource.id
+  http_method = aws_api_gateway_method.restaurante34-api_delete_method.http_method
+  type        = "AWS_PROXY"
+  integration_http_method = "POST"
+  uri         = aws_lambda_function.terraform_delete.invoke_arn
+}
+
+resource "aws_lambda_permission" "terraform_lambda_permission_delete" {
+  statement_id  = "AllowAPIGatewayInvoke"
+  action        = "lambda:InvokeFunction"
+  function_name = aws_lambda_function.terraform_delete.function_name
+  principal     = "apigateway.amazonaws.com"
+
+  source_arn = "${aws_api_gateway_rest_api.restaurante34-api.execution_arn}/*/*"
+}
+
 resource "aws_lambda_permission" "terraform_lambda_permission_signUp" {
   statement_id  = "AllowAPIGatewayInvoke"
   action        = "lambda:InvokeFunction"
@@ -162,6 +192,13 @@ provider "aws" {
   region  = "us-east-1"
   access_key = var.AWS_ACCESS_KEY_ID
   secret_key = var.AWS_SECRET_ACCESS_KEY
+}
+
+data "archive_file" "lambda_terraform_delete" {
+  type = "zip"
+
+  source_dir  = "${path.module}/delete"
+  output_path = "${path.module}/delete.zip"
 }
 
 data "archive_file" "lambda_terraform_getUsers" {
@@ -244,6 +281,22 @@ resource "aws_lambda_function" "terraform_getUsers" {
   runtime = "nodejs20.x"
   handler = "index.handler"
   source_code_hash = data.archive_file.lambda_terraform_getUsers.output_base64sha256
+  role             = aws_iam_role.lambda_exec.arn
+
+  environment {
+      variables = {
+        "clienteID" = var.AWS_COGNITO_CLIENT_ID,
+        "apiUrl" = var.API_URL
+      }
+  }
+}
+
+resource "aws_lambda_function" "terraform_delete" {
+  function_name = "delete"
+  filename      = "delete.zip"
+  runtime = "nodejs20.x"
+  handler = "index.handler"
+  source_code_hash = data.archive_file.lambda_terraform_delete.output_base64sha256
   role             = aws_iam_role.lambda_exec.arn
 
   environment {
